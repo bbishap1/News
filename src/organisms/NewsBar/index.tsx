1879a3df85156415ee9d505aee5c5a74e762d3d0
bishap <bbhusal@outcodesoftware.com>
2021-03-13 21:00:16 +0545

    fixes modal error

--- src/organisms/NewsBar/index.tsx
+++ src/organisms/NewsBar/index.tsx
@@ -83,7 +83,12 @@ const NewsBar: React.FC<Props> = (props) => {
   const { data, mainHeadline } = props;
 
   const toggleModal = (id: Number | null) => {
-    setModal({ show: !modal.show, id: id });
+    if(typeof(id) == "number") {
+      setModal({ show: !modal.show, id: id });
+    }
+    else {
+      setModal({show: false, id: null })
+    }
   };
   return (
     <div className={classes.newsBarWrapper}>
@@ -110,21 +115,21 @@ const NewsBar: React.FC<Props> = (props) => {
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0HJOYlW6Iqdh9WnP8VTZjRCuC33isLiQbKA&usqp=CAU"
           className={classes.sideNews}
           heading="What is Lorem Ipsum?"
-          // onClick={() => toggleModal()}
+          onClick={() => toggleModal(null)}
           description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
         />{" "}
         <Divider />
         <NewsBox
           heading="What is Lorem Ipsum?"
           className={classes.sideNews}
-          // onClick={() => toggleModal()}
+          onClick={() => toggleModal(null)}
           description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
         />{" "}
         <Divider />
         <NewsBox
           heading="What is data in the IT?"
           className={classes.sideNews}
-          // onClick={() => toggleModal()}
+          onClick={() => toggleModal(null)}
           description="This is description for data which is also short"
         />
       </div>
